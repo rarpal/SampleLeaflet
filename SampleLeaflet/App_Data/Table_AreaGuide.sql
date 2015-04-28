555f45076d3415b6d41d7a957ba705c3bf9b3be3
rarpal <rarpal@gmail.com>
2015-04-28 01:01:45 +0100

    code refactoring at home

---
+++ SampleLeaflet/App_Data/Table_AreaGuide.sql
@@ -0,0 +1,27 @@
+USE [jdGridDB]
+GO
+
+/****** Object:  Table [dbo].[AreaGuide]    Script Date: 27/04/2015 23:41:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[AreaGuide](
+	[AreaID] [varchar](10) NULL,
+	[Type] [varchar](3) NULL,
+	[AvgPrice] [money] NULL,
+	[Rating] [tinyint] NULL,
+	[Notes] [nvarchar](1000) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
